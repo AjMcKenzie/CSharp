8026a93c959a83efed1f05e962d9e5770add1372
Andrew <heldiggris7@gmail.com>
2018-01-16 14:32:19 -0600

    update Page29

---
+++ Page29/Page29/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Page29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Line one");
+            System.Console.WriteLine("Line two");
+            System.Console.WriteLine("Line three");
+        }
+    }
+}
