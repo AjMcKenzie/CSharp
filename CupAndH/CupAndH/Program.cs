836e032b9fcb48e0c9ad933799da1e9e8f3da0e1
Andrew <heldiggris7@gmail.com>
2018-01-16 14:54:13 -0600

    update CupAndH

---
+++ CupAndH/CupAndH/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CupAndH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("");
+            System.Console.WriteLine(" XXXXXXXXXXXXXX");
+            System.Console.WriteLine("  X          X ");
+            System.Console.WriteLine("   X       X   ");
+            System.Console.WriteLine("    XXXXXXX    ");
+            System.Console.WriteLine("       X       ");
+            System.Console.WriteLine("       X       ");
+            System.Console.WriteLine("     XXXXX");
+            System.Console.WriteLine("");
+
+            System.Console.WriteLine(" H       H");
+            System.Console.WriteLine(" H       H");
+            System.Console.WriteLine(" H       H");
+            System.Console.WriteLine(" HHHHHHHHH");
+            System.Console.WriteLine(" H       H");
+            System.Console.WriteLine(" H       H");
+            System.Console.WriteLine(" H       H");
+            System.Console.WriteLine("");
+
+            System.Console.WriteLine("A shave");
+            System.Console.WriteLine("That's real");
+            System.Console.WriteLine("No cuts to heal");
+            System.Console.WriteLine("A soothing");
+            System.Console.WriteLine("Velvet after-feel");
+            System.Console.WriteLine("Burma Shave");
+            System.Console.WriteLine("");
+
+
+        }
+    }
+}
