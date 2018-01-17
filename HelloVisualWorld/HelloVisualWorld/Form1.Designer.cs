b4c9c783a14e67876e529a57d61e2aa61a09b2cd
Andrew <heldiggris7@gmail.com>
2018-01-17 13:15:07 -0600

    update HelloVisualWorld

---
+++ HelloVisualWorld/HelloVisualWorld/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace HelloVisualWorld
+{
+    partial class HelloForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.helloLable = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(100, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click here";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // helloLable
+            // 
+            this.helloLable.AutoSize = true;
+            this.helloLable.Location = new System.Drawing.Point(90, 75);
+            this.helloLable.Name = "helloLable";
+            this.helloLable.Size = new System.Drawing.Size(99, 13);
+            this.helloLable.TabIndex = 1;
+            this.helloLable.Text = "Hello, Visual World!";
+            this.helloLable.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(100, 179);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Click me last";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // HelloForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.helloLable);
+            this.Controls.Add(this.button1);
+            this.Name = "HelloForm";
+            this.Text = "Hello Visual World";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label helloLable;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ HelloVisualWorld/HelloVisualWorld/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloVisualWorld
+{
+    public partial class HelloForm : Form
+    {
+        public HelloForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            helloLable.Visible = true;
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            helloLable.Text = "Goodbye";
+        }
+    }
+}
