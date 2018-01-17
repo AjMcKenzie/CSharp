c47753b9a4e6555d3fc9dd627c0df338b857d515
Andrew <heldiggris7@gmail.com>
2018-01-17 14:00:54 -0600

    update comareNumbers

---
+++ CompareThreeNumbers/CompareThreeNumbers/CompareThreeNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompareThreeNumbers
+{
+    class CompareThreeNumbers
+    {
+        static void Main(string[] args)
+        {
+            string numberString;
+            int num1, num2, num3;
+
+            System.Console.Write("Enter an integer ");
+            numberString = System.Console.ReadLine();
+            num1 = Convert.ToInt32(numberString);
+
+        }
+    }
+}
