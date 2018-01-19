3a7d4254f2f8410bd6d811a870ba2aba0cebfbbf
Andrew <heldiggris7@gmail.com>
2018-01-19 13:56:12 -0600

    update DailySpecial

--- DailySpecial/DailySpecial/Form1.cs
+++ DailySpecial/DailySpecial/Form1.cs
@@ -25,6 +25,32 @@ namespace DailySpecial
         private void btnSpecial_Click(object sender, EventArgs e)
         {
             int day = Convert.ToInt32(dayBox.Text);
+            String special;
+
+            switch ((Day)day)
+            {
+                case Day.SUNDAY:
+                    special = "fried chicken";
+                    break;
+                case Day.MONDAY:
+                    special = "Sorry-Closed";
+                    break;
+                case Day.TUESDAY:
+                case Day.WEDNESDAY:
+                case Day.THURSDAY:
+                    special = "meat loaf";
+                    break;
+                case Day.FRIDAY:
+                    special = "fish frys";
+                    break;
+                case Day.SATURDAY:
+                    special = "liver and onions";
+                    break;
+                default:
+                    special = "Invalid day";
+                    break;
+            }
+            lblOutPut.Text = "Day-"+Day +"Today's special is " + special;
         }
         
            
