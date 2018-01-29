6fc3f1f8d6d7ce0b8e352d1c9ed109b0d0686cac
Andrew <heldiggris7@gmail.com>
2018-01-29 14:34:16 -0600

    update GuessingGameGUI

--- GuessingGameGUI/GuessingGameGUI/Form1.Designer.cs
+++ GuessingGameGUI/GuessingGameGUI/Form1.Designer.cs
@@ -46,11 +46,12 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(98, 67);
+            this.label1.Location = new System.Drawing.Point(101, 44);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(150, 24);
+            this.label1.Size = new System.Drawing.Size(150, 48);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Guess a number";
+            this.label1.Text = "Guess a number\r\nBetween 1-10\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // lblOutput
             // 
